b57cc0b45d580e710356edf1f8fa37d8daaa3f88
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-06-09 14:25:10 -0700

    Update league/team models

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class League < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :teams
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Team < ApplicationRecord
+  validates :name, presence: true
+  validates :league_id, presence: true
+
+  belongs_to :league
 end
