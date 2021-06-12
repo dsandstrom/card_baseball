71547bbaafc544dce2983b682de8560a1a104038
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-06-12 16:34:26 -0700

    Add menu_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def menu_link(text, path)
+    klass = 'menu-link'
+    klass += ' current-page' if current_page?(path)
+
+    content_tag :p do
+      link_to(text, path, class: klass)
+    end
+  end
+
+  private
+
+    def enable_page_title(title)
+      title = "Card Baseball | #{title}"
+      content_for(:title, truncate(title, length: 70, omission: ''))
+    end
 end
