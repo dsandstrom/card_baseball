93ed9f54ef7041ac49b38506b084a33eee140830
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-06-11 17:54:56 -0700

    Add League specs

--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -3,5 +3,16 @@
 require "rails_helper"
 
 RSpec.describe League, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @league = League.new(name: "National")
+  end
+
+  subject { @league }
+
+  it { is_expected.to be_valid }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+
+  it { is_expected.to have_many(:teams) }
 end
