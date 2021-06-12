0d0a1a4f7ec02b71332ac41972fa72268ca7ae96
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-06-11 17:06:50 -0700

    Add breadcrumbs helper

---
+++ app/helpers/breadcrumbs_helper.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module BreadcrumbsHelper
+  def breadcrumbs(views = nil)
+    return unless views
+
+    content_tag :div, class: 'breadcrumbs' do
+      views.each do |text, url, options|
+        concat breadcrumb(text, url, options)
+      end
+    end
+  end
+
+  private
+
+    def project_breadcrumb_item(project)
+      [project.name, project_path(project)]
+    end
+
+    def breadcrumb(text, url, options = {})
+      content_tag :span, class: 'breadcrumb' do
+        link_to text, url, options
+      end
+    end
+end
