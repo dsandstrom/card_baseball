dd5ec2d1756971ac68bca2c72157277a5f9e14f7
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-06-13 14:21:49 -0700

    Add TeamsControllerSpec

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,70 +6,44 @@ class TeamsController < ApplicationController
   before_action :set_league, except: :index
   before_action :set_team, only: %i[show edit update destroy]
 
-  # GET /teams or /teams.json
   def index
     @teams = Team.all
   end
 
-  # GET /teams/1 or /teams/1.json
   def show; end
 
-  # GET /teams/new
   def new
     @team = @league.teams.build
   end
 
-  # GET /teams/1/edit
   def edit; end
 
-  # POST /teams or /teams.json
   def create
     @team = @league.teams.build(team_params)
 
-    respond_to do |format|
-      if @team.save
-        format.html do
-          redirect_to league_team_path(@league, @team),
-                      notice: 'Team was successfully created.'
-        end
-        format.json { render :show, status: :created, location: @team }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
+    if @team.save
+      redirect_to @league, notice: 'Team was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /teams/1 or /teams/1.json
   def update
-    respond_to do |format|
-      if @team.update(team_params)
-        format.html do
-          redirect_to league_team_path(@league, @team),
-                      notice: 'Team was successfully updated.'
-        end
-        format.json { render :show, status: :ok, location: @team }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @team.errors, status: :unprocessable_entity }
-      end
+    if @team.update(team_params)
+      redirect_to league_team_path(@league, @team),
+                  notice: 'Team was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /teams/1 or /teams/1.json
   def destroy
     @team.destroy
-    respond_to do |format|
-      format.html do
-        redirect_to @league, notice: 'Team was successfully destroyed.'
-      end
-      format.json { head :no_content }
-    end
+    redirect_to @league, notice: 'Team was successfully destroyed.'
   end
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_league
       @league = League.find(params[:league_id])
     end
@@ -78,7 +52,6 @@ class TeamsController < ApplicationController
       @team = @league.teams.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def team_params
       params.require(:team).permit(:name, :icon)
     end
---
+++ spec/controllers/teams_controller_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TeamsController, type: :controller do
+  let(:league) { Fabricate(:league) }
+  let(:team) { Fabricate(:team, league: league) }
+
+  let(:valid_attributes) { { name: "Name" } }
+  let(:invalid_attributes) { { name: "" } }
+
+  describe "GET #show" do
+    it "returns a success response" do
+      get :show, params: { league_id: league.to_param,
+                           id: team.to_param }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #new" do
+    it "returns a success response" do
+      get :new, params: { league_id: league.to_param }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns a success response" do
+      get :edit, params: { league_id: league.to_param,
+                           id: team.to_param }
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    context "when valid params" do
+      it "creates a new Team" do
+        expect do
+          post :create, params: { league_id: league.to_param,
+                                  team: valid_attributes }
+        end.to change(Team, :count).by(1)
+      end
+
+      it "redirects to the Team list" do
+        post :create, params: { league_id: league.to_param,
+                                team: valid_attributes }
+        expect(response).to redirect_to(league)
+      end
+    end
+
+    context "when invalid params" do
+      it "doesn't create a new Team" do
+        expect do
+          post :create, params: { league_id: league.to_param,
+                                  team: invalid_attributes }
+        end.not_to change(Team, :count)
+      end
+
+      it "renders new" do
+        post :create, params: { league_id: league.to_param,
+                                team: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "when valid params" do
+      it "updates the requested Team" do
+        expect do
+          put :update, params: { league_id: league.to_param,
+                                 id: team.to_param,
+                                 team: valid_attributes }
+          team.reload
+        end.to change(team, :name)
+      end
+
+      it "redirects to the Team" do
+        put :update, params: { league_id: league.to_param,
+                               id: team.to_param,
+                               team: valid_attributes }
+        expect(response).to redirect_to(league_team_path(league, team))
+      end
+    end
+
+    context "when invalid params" do
+      it "doesn't create a new Team" do
+        expect do
+          put :update, params: { league_id: league.to_param,
+                                 id: team.to_param,
+                                 team: invalid_attributes }
+          team.reload
+        end.not_to change(team, :name)
+      end
+
+      it "renders edit" do
+        put :update, params: { league_id: league.to_param,
+                               id: team.to_param,
+                               team: invalid_attributes }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before { team }
+
+    it "destroys the requested Team" do
+      expect do
+        delete :destroy, params: { league_id: league.to_param,
+                                   id: team.to_param }
+      end.to change(Team, :count).by(-1)
+    end
+
+    it "redirects to the Team list" do
+      delete :destroy, params: { league_id: league.to_param,
+                                 id: team.to_param }
+      expect(response).to redirect_to(league)
+    end
+  end
+end
