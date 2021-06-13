679b7756bcce569f7baebfecb8ce275f32c709c8
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-06-13 13:03:35 -0700

    Fix line lengths for rubocop

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,8 @@ threads min_threads_count, max_threads_count
 #
 worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+# Specifies the `port` that Puma will listen on to receive requests; default is
+# 3000.
 #
 port ENV.fetch('PORT', 3000)
 
@@ -28,9 +29,8 @@ pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
+# the concurrency of the application would be max `threads` * `workers`. Workers
+# do not work on JRuby or Windows (both of which do not support processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
