eed1c6ab97a8fca7cba3ea13785ffed35d1a5df2
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-06-23 00:30:43 -0700

    Add radio_buttons.js
    Activates labels when buttons hidden

---
+++ app/javascript/packs/radio_buttons.js
@@ -0,0 +1,59 @@
+class RadioButtonLabel {
+  constructor(elem) {
+    this.elem = elem;
+    this.radio = this.elem.querySelector('input');
+    this.elem.classList.add('roller-radio-button-label');
+    this.toggleDisabledClass();
+  }
+
+  checked() { return this.radio.checked; }
+
+  toggleDisabledClass() {
+    if (this.checked()) {
+      this.elem.classList.remove('disabled');
+      this.elem.classList.add('active');
+    } else {
+      this.elem.classList.add('disabled');
+      this.elem.classList.remove('active');
+    }
+  }
+}
+
+class RadioButtons {
+  constructor(elem) {
+    this.labels = [];
+    for (var label of elem.querySelectorAll('label')) {
+      this.labels.push(new RadioButtonLabel(label));
+    }
+    if (!this.labels.length) return;
+
+    this.watchForChanges();
+  }
+
+  watchForChanges() {
+    for (var label of this.labels) {
+      const radio = label.elem.querySelector('input');
+      if (!radio) return;
+
+      radio.addEventListener('change', () => {
+        this.toggleDisabledClasses();
+      });
+    }
+  }
+
+  toggleDisabledClasses() {
+    for (var label of this.labels) {
+      label.toggleDisabledClass();
+    }
+  }
+}
+
+const radioButtonClasses = ['spot-hitter-positions'];
+
+document.addEventListener('turbolinks:load', function() {
+  for (let klass of radioButtonClasses) {
+    document.querySelectorAll('.' + klass).forEach((radioButtons) => {
+      new RadioButtons(radioButtons);
+    });
+  }
+});
