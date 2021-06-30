52eaf77ee859a7b06f8732f036f254749c968eb8
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-06-30 16:45:11 -0700

    Work on SpotsController
    Add js responses
    Allow bench hitter to be moved to empty spot
    Allow lineup hitter to be moved to empty spot
    Allow switching spots with another hitter while keeping respective
    positions

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -12,28 +12,84 @@ class SpotsController < ApplicationController
 
   def create
     @spot = @lineup.spots.build(spot_params)
+    @hitter = @spot.hitter
 
-    if @spot.save
-      redirect_to redirect_url,
-                  notice: 'Batting spot was successfully set.'
-    else
-      render :new
+    if @hitter
+      old_spot = @lineup.spots.find_by(hitter_id: @hitter.id)
+      if old_spot
+        @spot.position = old_spot.position
+        @new_spot = @lineup.spots.build(batting_order: old_spot.batting_order)
+        old_spot.destroy
+      end
+    end
+
+    respond_to do |format|
+      format.html do
+        if @spot.save
+          redirect_to redirect_url,
+                      notice: 'Batting spot was successfully set.'
+        else
+          render :new
+        end
+      end
+      format.js do
+        if @spot.save
+          render :show
+        else
+          render :new
+        end
+      end
     end
   end
 
   def update
-    if @spot.update(spot_params)
-      redirect_to redirect_url,
-                  notice: 'Batting spot was successfully changed.'
-    else
-      render :edit
+    respond_to do |format|
+      format.html do
+        if @spot.update(update_params)
+          redirect_to redirect_url,
+                      notice: 'Batting spot was successfully changed.'
+        else
+          render :edit
+        end
+      end
+      format.js do
+        old_hitter = @spot.hitter
+        old_position = @spot.position
+        @spot.assign_attributes(update_params)
+        @old_spot = @lineup.spots.find_by(hitter_id: @spot.hitter_id)
+
+        unless old_hitter == @spot.hitter
+          if @old_spot
+            @spot.position = @old_spot.position
+            @old_spot.update_columns(hitter_id: old_hitter.id,
+                                     position: old_position)
+          else
+            @bench_hitter = old_hitter
+          end
+        end
+
+        if @spot.save
+          render :show
+        else
+          render :edit
+        end
+      end
     end
   end
 
   def destroy
+    @bench_hitter = @spot.hitter
+    batting_order = @spot.batting_order
     @spot.destroy
-    redirect_to redirect_url,
-                notice: 'Batting spot was successfully cleared.'
+    respond_to do |format|
+      format.html do
+        redirect_to redirect_url,
+                    notice: 'Batting spot was successfully cleared.'
+      end
+      format.js do
+        @spot = @lineup.spots.build(batting_order: batting_order)
+      end
+    end
   end
 
   private
@@ -50,6 +106,10 @@ class SpotsController < ApplicationController
 
     def spot_params
       params.require(:spot).permit(:hitter_id, :position, :batting_order)
+    end
+
+    def update_params
+      params.require(:spot).permit(:hitter_id, :position)
     end
 
     def redirect_url
--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -4,100 +4,420 @@ require "rails_helper"
 
 RSpec.describe SpotsController, type: :controller do
   let(:team) { Fabricate(:team) }
-  let(:hitter) { Fabricate(:hitter, catcher_defense: 5) }
-  let(:lineup) { Fabricate(:lineup, team: team) }
-  let(:spot) { Fabricate(:spot, lineup: lineup, batting_order: 4) }
-
-  before do
-    Fabricate(:hitter_contract, hitter: hitter, team: team)
+  let(:hitter) do
+    Fabricate(:hitter, catcher_defense: 5, first_base_defense: 1)
   end
+  let(:lineup) { Fabricate(:lineup, team: team) }
+  let(:spot) do
+    Fabricate(:spot, lineup: lineup, hitter: hitter, batting_order: 4,
+                     position: 2)
+  end
+
+  before { Fabricate(:hitter_contract, hitter: hitter, team: team) }
 
   let(:valid_attributes) do
     { hitter_id: hitter.to_param, position: 2, batting_order: 2 }
   end
-  let(:invalid_attributes) { { batting_order: "" } }
+
+  let(:update_attributes) { { position: 3 } }
+  let(:invalid_attributes) { { position: "" } }
 
   describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: { lineup_id: lineup.to_param }
-      expect(response).to be_successful
+    context "for an HTML request" do
+      it "returns a success response" do
+        get :new, params: { lineup_id: lineup.to_param }
+        expect(response).to be_successful
+      end
+    end
+
+    context "for a JS request" do
+      it "returns a success response" do
+        get :new, params: { lineup_id: lineup.to_param }, xhr: true
+        expect(response).to be_successful
+      end
     end
   end
 
   describe "GET #edit" do
-    it "returns a success response" do
-      get :edit, params: { lineup_id: lineup.to_param,
-                           id: spot.to_param }
-      expect(response).to be_successful
+    context "for an HTML request" do
+      it "returns a success response" do
+        get :edit, params: { lineup_id: lineup.to_param, id: spot.to_param }
+        expect(response).to be_successful
+      end
+    end
+
+    context "for a JS request" do
+      it "returns a success response" do
+        get :edit, params: { lineup_id: lineup.to_param, id: spot.to_param },
+                   xhr: true
+        expect(response).to be_successful
+      end
     end
   end
 
   describe "POST #create" do
-    context "when valid params" do
-      it "creates a new Spot" do
-        expect do
+    context "for an HTML request" do
+      context "when valid params" do
+        it "creates a new Spot" do
+          expect do
+            post :create, params: { lineup_id: lineup.to_param,
+                                    spot: valid_attributes }
+          end.to change(Spot, :count).by(1)
+        end
+
+        it "redirects to the Spot list" do
           post :create, params: { lineup_id: lineup.to_param,
                                   spot: valid_attributes }
-        end.to change(Spot, :count).by(1)
+          expect(response).to redirect_to(team_lineup_path(team, lineup))
+        end
       end
 
-      it "redirects to the Spot list" do
-        post :create, params: { lineup_id: lineup.to_param,
-                                spot: valid_attributes }
-        expect(response).to redirect_to(team_lineup_path(team, lineup))
+      context "when invalid params" do
+        it "doesn't create a new Spot" do
+          expect do
+            post :create, params: { lineup_id: lineup.to_param,
+                                    spot: invalid_attributes }
+          end.not_to change(Spot, :count)
+        end
+
+        it "renders new" do
+          post :create, params: { lineup_id: lineup.to_param,
+                                  spot: invalid_attributes }
+          expect(response).to be_successful
+        end
       end
     end
 
-    context "when invalid params" do
-      it "doesn't create a new Spot" do
-        expect do
+    context "for a JS request" do
+      context "when valid params" do
+        it "creates a new Spot" do
+          expect do
+            post :create, params: { lineup_id: lineup.to_param,
+                                    spot: valid_attributes },
+                          xhr: true
+          end.to change(Spot, :count).by(1)
+        end
+
+        it "redirects to the Spot list" do
           post :create, params: { lineup_id: lineup.to_param,
-                                  spot: invalid_attributes }
-        end.not_to change(Spot, :count)
+                                  spot: valid_attributes },
+                        xhr: true
+          expect(response).to be_successful
+        end
       end
 
-      it "renders new" do
-        post :create, params: { lineup_id: lineup.to_param,
-                                spot: invalid_attributes }
-        expect(response).to be_successful
+      context "when invalid params" do
+        it "doesn't create a new Spot" do
+          expect do
+            post :create, params: { lineup_id: lineup.to_param,
+                                    spot: invalid_attributes },
+                          xhr: true
+          end.not_to change(Spot, :count)
+        end
+
+        it "renders new" do
+          post :create, params: { lineup_id: lineup.to_param,
+                                  spot: invalid_attributes },
+                        xhr: true
+          expect(response).to be_successful
+        end
+      end
+
+      context "when hitter has a current spot in the lineup" do
+        before do
+          Fabricate(:spot, lineup: lineup, hitter: hitter, batting_order: 1,
+                           position: 2)
+        end
+
+        let(:valid_attributes) do
+          { hitter_id: hitter.to_param, batting_order: 2 }
+        end
+
+        it "doesn't change the overall Spot's count" do
+          expect do
+            post :create, params: { lineup_id: lineup.to_param,
+                                    spot: valid_attributes },
+                          xhr: true
+          end.not_to change(Spot, :count)
+        end
+
+        it "destroys the old spot" do
+          old_spot = lineup.spots.find_by(hitter_id: hitter.id)
+          expect(old_spot).not_to be_nil
+          old_spot_id = old_spot.id
+
+          post :create, params: { lineup_id: lineup.to_param,
+                                  spot: valid_attributes },
+                        xhr: true
+          expect(Spot.find_by(id: old_spot_id)).to be_nil
+        end
+
+        it "creates a new spot" do
+          post :create, params: { lineup_id: lineup.to_param,
+                                  spot: valid_attributes },
+                        xhr: true
+          new_spot = Spot.last
+          expect(new_spot).not_to be_nil
+          expect(new_spot.hitter_id).to eq(hitter.id)
+          expect(new_spot.batting_order).to eq(2)
+          expect(new_spot.position).to eq(2)
+        end
+
+        it "renders show" do
+          post :create, params: { lineup_id: lineup.to_param,
+                                  spot: valid_attributes },
+                        xhr: true
+          expect(response).to be_successful
+        end
       end
     end
   end
 
   describe "PUT #update" do
-    context "when valid params" do
-      it "updates the requested Spot" do
-        expect do
+    before { spot }
+
+    context "for an HTML request" do
+      context "when valid params" do
+        it "updates the requested Spot" do
+          expect do
+            put :update, params: { lineup_id: lineup.to_param,
+                                   id: spot.to_param,
+                                   spot: update_attributes }
+            spot.reload
+          end.to change(spot, :position)
+        end
+
+        it "redirects to the Spot" do
           put :update, params: { lineup_id: lineup.to_param,
                                  id: spot.to_param,
-                                 spot: valid_attributes }
-          spot.reload
-        end.to change(spot, :batting_order)
+                                 spot: update_attributes }
+          expect(response).to redirect_to(team_lineup_path(team, lineup))
+        end
       end
 
-      it "redirects to the Spot" do
-        put :update, params: { lineup_id: lineup.to_param,
-                               id: spot.to_param,
-                               spot: valid_attributes }
-        expect(response).to redirect_to(team_lineup_path(team, lineup))
-      end
-    end
+      context "when invalid params" do
+        it "doesn't change the requested Spot's name" do
+          expect do
+            put :update, params: { lineup_id: lineup.to_param,
+                                   id: spot.to_param,
+                                   spot: invalid_attributes }
+            spot.reload
+          end.not_to change(spot, :position)
+        end
 
-    context "when invalid params" do
-      it "doesn't change the requested Spot's name" do
-        expect do
+        it "renders edit" do
           put :update, params: { lineup_id: lineup.to_param,
                                  id: spot.to_param,
                                  spot: invalid_attributes }
-          spot.reload
-        end.not_to change(spot, :batting_order)
+          expect(response).to be_successful
+        end
+      end
+    end
+
+    context "for a JS request" do
+      context "when valid params" do
+        it "updates the requested Spot" do
+          expect do
+            put :update, params: { lineup_id: lineup.to_param,
+                                   id: spot.to_param,
+                                   spot: update_attributes },
+                         xhr: true
+            spot.reload
+          end.to change(spot, :position)
+        end
+
+        it "redirects to the Spot" do
+          put :update, params: { lineup_id: lineup.to_param,
+                                 id: spot.to_param,
+                                 spot: update_attributes },
+                       xhr: true
+          expect(response).to be_successful
+        end
       end
 
-      it "renders edit" do
-        put :update, params: { lineup_id: lineup.to_param,
-                               id: spot.to_param,
-                               spot: invalid_attributes }
-        expect(response).to be_successful
+      context "when the same params" do
+        let(:update_attributes) do
+          { hitter_id: spot.hitter_id, position: spot.position }
+        end
+
+        it "updates the requested Spot" do
+          expect do
+            put :update, params: { lineup_id: lineup.to_param,
+                                   id: spot.to_param,
+                                   spot: update_attributes },
+                         xhr: true
+            spot.reload
+          end.not_to change(spot, :hitter_id)
+        end
+
+        it "redirects to the Spot" do
+          put :update, params: { lineup_id: lineup.to_param,
+                                 id: spot.to_param,
+                                 spot: update_attributes },
+                       xhr: true
+          expect(response).to be_successful
+        end
+      end
+
+      context "when invalid params" do
+        it "doesn't change the requested Spot's name" do
+          expect do
+            put :update, params: { lineup_id: lineup.to_param,
+                                   id: spot.to_param,
+                                   spot: invalid_attributes },
+                         xhr: true
+            spot.reload
+          end.not_to change(spot, :position)
+        end
+
+        it "renders edit" do
+          put :update, params: { lineup_id: lineup.to_param,
+                                 id: spot.to_param,
+                                 spot: invalid_attributes },
+                       xhr: true
+          expect(response).to be_successful
+        end
+      end
+
+      context "when switching hitters" do
+        context "from the bench" do
+          context "when hitter plays spot's position" do
+            let(:bench_hitter) { Fabricate(:hitter, catcher_defense: -1) }
+            let(:update_attributes) { { hitter_id: bench_hitter.to_param } }
+
+            before do
+              Fabricate(:hitter_contract, hitter: bench_hitter, team: team)
+            end
+
+            it "updates the requested Spot" do
+              expect do
+                put :update, params: { lineup_id: lineup.to_param,
+                                       id: spot.to_param,
+                                       spot: update_attributes },
+                             xhr: true
+                spot.reload
+              end.to change(spot, :hitter_id).to(bench_hitter.id)
+            end
+
+            it "doesn't change overall Spot count" do
+              expect do
+                put :update, params: { lineup_id: lineup.to_param,
+                                       id: spot.to_param,
+                                       spot: update_attributes },
+                             xhr: true
+              end.not_to change(Spot, :count)
+            end
+
+            it "redirects to the Spot" do
+              put :update, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param,
+                                     spot: update_attributes },
+                           xhr: true
+              expect(response).to be_successful
+            end
+          end
+
+          context "when hitter doesn't play spot's position" do
+            let(:bench_hitter) { Fabricate(:hitter, first_base_defense: 9) }
+            let(:update_attributes) { { hitter_id: bench_hitter.to_param } }
+
+            before do
+              Fabricate(:hitter_contract, hitter: bench_hitter, team: team)
+            end
+
+            it "updates the requested Spot" do
+              expect do
+                put :update, params: { lineup_id: lineup.to_param,
+                                       id: spot.to_param,
+                                       spot: update_attributes },
+                             xhr: true
+                spot.reload
+              end.not_to change(spot, :hitter_id)
+            end
+
+            it "renders edit" do
+              put :update, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param,
+                                     spot: update_attributes },
+                           xhr: true
+              expect(response).to be_successful
+            end
+          end
+        end
+
+        context "from another spot in the requested lineup" do
+          let(:new_hitter) do
+            Fabricate(:hitter, catcher_defense: 10, first_base_defense: 0)
+          end
+
+          before do
+            Fabricate(:hitter_contract, hitter: new_hitter, team: team)
+          end
+
+          let!(:old_spot) do
+            Fabricate(:spot, lineup: lineup, hitter: new_hitter, position: 3,
+                             batting_order: 7)
+          end
+
+          let(:update_attributes) { { hitter_id: new_hitter.to_param } }
+
+          it "updates the requested Spot's hitter" do
+            expect do
+              put :update, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param,
+                                     spot: update_attributes },
+                           xhr: true
+              spot.reload
+            end.to change(spot, :hitter_id).to(new_hitter.id)
+          end
+
+          it "updates the requested Spot's position" do
+            expect do
+              put :update, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param,
+                                     spot: update_attributes },
+                           xhr: true
+              spot.reload
+            end.to change(spot, :position).to(3)
+          end
+
+          it "updates the old Spot's hitter" do
+            expect do
+              put :update, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param,
+                                     spot: update_attributes },
+                           xhr: true
+              old_spot.reload
+            end.to change(old_spot, :hitter_id).to(hitter.id)
+          end
+
+          it "updates the old Spot's position" do
+            expect do
+              put :update, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param,
+                                     spot: update_attributes },
+                           xhr: true
+              old_spot.reload
+            end.to change(old_spot, :position).to(2)
+          end
+
+          it "doesn't change overall Spot count" do
+            expect do
+              put :update, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param,
+                                     spot: update_attributes },
+                           xhr: true
+            end.not_to change(Spot, :count)
+          end
+
+          it "redirects to the Spot" do
+            put :update, params: { lineup_id: lineup.to_param,
+                                   id: spot.to_param,
+                                   spot: update_attributes },
+                         xhr: true
+            expect(response).to be_successful
+          end
+        end
       end
     end
   end
@@ -105,17 +425,36 @@ RSpec.describe SpotsController, type: :controller do
   describe "DELETE #destroy" do
     before { spot }
 
-    it "destroys the requested Spot" do
-      expect do
+    context "for an HTML request" do
+      it "destroys the requested Spot" do
+        expect do
+          delete :destroy, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param }
+        end.to change(Spot, :count).by(-1)
+      end
+
+      it "redirects to the Spot list" do
         delete :destroy, params: { lineup_id: lineup.to_param,
                                    id: spot.to_param }
-      end.to change(Spot, :count).by(-1)
+        expect(response).to redirect_to(team_lineup_path(team, lineup))
+      end
     end
 
-    it "redirects to the Spot list" do
-      delete :destroy, params: { lineup_id: lineup.to_param,
-                                 id: spot.to_param }
-      expect(response).to redirect_to(team_lineup_path(team, lineup))
+    context "for a JS request" do
+      it "destroys the requested Spot" do
+        expect do
+          delete :destroy, params: { lineup_id: lineup.to_param,
+                                     id: spot.to_param },
+                           xhr: true
+        end.to change(Spot, :count).by(-1)
+      end
+
+      it "redirects to the Spot list" do
+        delete :destroy, params: { lineup_id: lineup.to_param,
+                                   id: spot.to_param },
+                         xhr: true
+        expect(response).to be_successful
+      end
     end
   end
 end
