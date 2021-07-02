d3c9aab12df873ecdfeb7193cd33e20945f043ec
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-07-02 00:02:37 -0700

    Refactor SpotsController

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -12,68 +12,18 @@ class SpotsController < ApplicationController
 
   def create
     @spot = @lineup.spots.build(spot_params)
-    @hitter = @spot.hitter
-
-    if @hitter
-      old_spot = @lineup.spots.find_by(hitter_id: @hitter.id)
-      if old_spot
-        @spot.position = old_spot.position
-        @new_spot = @lineup.spots.build(batting_order: old_spot.batting_order)
-        old_spot.destroy
-      end
-    end
+    move_from_another_spot
 
     respond_to do |format|
-      format.html do
-        if @spot.save
-          redirect_to redirect_url,
-                      notice: 'Batting spot was successfully set.'
-        else
-          render :new
-        end
-      end
-      format.js do
-        if @spot.save
-          render :show
-        else
-          render :new
-        end
-      end
+      format.html { html_create_response }
+      format.js { js_create_response }
     end
   end
 
   def update
     respond_to do |format|
-      format.html do
-        if @spot.update(update_params)
-          redirect_to redirect_url,
-                      notice: 'Batting spot was successfully changed.'
-        else
-          render :edit
-        end
-      end
-      format.js do
-        old_hitter = @spot.hitter
-        old_position = @spot.position
-        @spot.assign_attributes(update_params)
-        @old_spot = @lineup.spots.find_by(hitter_id: @spot.hitter_id)
-
-        unless old_hitter == @spot.hitter
-          if @old_spot
-            @spot.position = @old_spot.position
-            @old_spot.update_columns(hitter_id: old_hitter.id,
-                                     position: old_position)
-          else
-            @bench_hitter = old_hitter
-          end
-        end
-
-        if @spot.save
-          render :show
-        else
-          render :edit
-        end
-      end
+      format.html { html_update_response }
+      format.js { js_update_response }
     end
   end
 
@@ -81,14 +31,13 @@ class SpotsController < ApplicationController
     @bench_hitter = @spot.hitter
     batting_order = @spot.batting_order
     @spot.destroy
+
     respond_to do |format|
       format.html do
         redirect_to redirect_url,
                     notice: 'Batting spot was successfully cleared.'
       end
-      format.js do
-        @spot = @lineup.spots.build(batting_order: batting_order)
-      end
+      format.js { @spot = @lineup.spots.build(batting_order: batting_order) }
     end
   end
 
@@ -112,7 +61,74 @@ class SpotsController < ApplicationController
       params.require(:spot).permit(:hitter_id, :position)
     end
 
+    def html_create_response
+      if @spot.save
+        redirect_to redirect_url,
+                    notice: 'Batting spot was successfully set.'
+      else
+        render :new
+      end
+    end
+
+    def js_create_response
+      if @spot.save
+        render :show
+      else
+        render :new
+      end
+    end
+
+    def html_update_response
+      if @spot.update(update_params)
+        redirect_to redirect_url,
+                    notice: 'Batting spot was successfully changed.'
+      else
+        render :edit
+      end
+    end
+
+    def js_update_response
+      update_spot
+
+      if @spot.save
+        render :show
+      else
+        render :edit
+      end
+    end
+
     def redirect_url
       team_lineup_path(@team, @lineup)
+    end
+
+    def move_from_another_spot
+      hitter = @spot.hitter
+      return unless hitter
+
+      old_spot = @lineup.spots.find_by(hitter_id: hitter.id)
+      return unless old_spot
+
+      @spot.position = old_spot.position
+      @new_spot = @lineup.spots.build(batting_order: old_spot.batting_order)
+      old_spot.destroy
+    end
+
+    def update_spot
+      old_hitter = @spot.hitter
+      old_position = @spot.position
+      @spot.assign_attributes(update_params)
+      @old_spot = @lineup.spots.find_by(hitter_id: @spot.hitter_id)
+      return if old_hitter == @spot.hitter
+
+      if @old_spot
+        switch_spots(old_hitter, old_position)
+      else
+        @bench_hitter = old_hitter
+      end
+    end
+
+    def switch_spots(hitter, position)
+      @spot.position = @old_spot.position
+      @old_spot.update_columns(hitter_id: hitter.id, position: position)
     end
 end
