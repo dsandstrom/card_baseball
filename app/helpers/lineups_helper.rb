0530f7db5d05855efd7e519e4b1cd2257b14c88e
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-07-02 19:51:04 -0700

    Show if lineup complete in partial

--- app/helpers/lineups_helper.rb
+++ app/helpers/lineups_helper.rb
@@ -9,6 +9,14 @@ module LineupsHelper
     content_tag(tag, value, options)
   end
 
+  def lineup_tags(lineup)
+    if lineup.complete?
+      content_tag(:span, 'Complete', class: 'tag tag-complete')
+    else
+      content_tag(:span, 'Incomplete', class: 'tag tag-incomplete')
+    end
+  end
+
   private
 
     def lineup_defense_class_adder(defense)
