ae126caccf3ae5fcfd32bb3c3a8a1ba88704a91d
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-07-03 17:43:12 -0700

    Update stylelint config
    Use sass guidelines
    customize rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,10 +3,14 @@
 const sortOrderSmacss = require('stylelint-config-property-sort-order-smacss/generate');
 
 module.exports = {
+  extends: 'stylelint-config-sass-guidelines',
   plugins: ['stylelint-order', 'stylelint-scss'],
   rules: {
-    'order/properties-order': [
-      sortOrderSmacss()
-    ]
-  },
+    'indentation': [2, { ignore: ['value'] }],
+    'order/properties-alphabetical-order': null,
+    'order/properties-order': [sortOrderSmacss()],
+    'max-nesting-depth': 4,
+    'number-leading-zero': null,
+    'selector-no-qualifying-type': [true, { ignore: ['class'] }]
+  }
 };
