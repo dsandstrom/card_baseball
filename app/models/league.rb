dabe1fd682e13f3b67d6262605115a7b3904d7fa
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-07-04 21:05:05 -0700

    Add League#first?/last?

--- app/models/league.rb
+++ app/models/league.rb
@@ -8,4 +8,12 @@ class League < ApplicationRecord
   has_many :teams, dependent: :nullify
 
   ranks :row_order
+
+  def first?
+    @first ||= row_order_rank.present? && row_order_rank.zero?
+  end
+
+  def last?
+    @last ||= row_order_rank.present? && row_order_rank + 1 == League.count
+  end
 end
--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -15,4 +15,64 @@ RSpec.describe League, type: :model do
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
 
   it { is_expected.to have_many(:teams) }
+
+  describe "#first?" do
+    context "when only League" do
+      let(:league) { Fabricate(:league) }
+
+      it "returns true" do
+        expect(league.first?).to eq(true)
+      end
+    end
+
+    context "when two Leagues" do
+      let!(:first_league) { Fabricate(:league) }
+      let!(:second_league) { Fabricate(:league) }
+
+      it "returns true for first ordered league" do
+        expect(first_league.first?).to eq(true)
+        expect(second_league.first?).to eq(false)
+      end
+    end
+
+    context "when row_order_rank is nil" do
+      let(:league) { Fabricate(:league) }
+
+      before { allow(league).to receive(:row_order_rank) { nil } }
+
+      it "returns false" do
+        expect(league.first?).to eq(false)
+      end
+    end
+  end
+
+  describe "#last?" do
+    context "when only League" do
+      let(:league) { Fabricate(:league) }
+
+      it "returns true" do
+        expect(league.last?).to eq(true)
+      end
+    end
+
+    context "when two Leagues" do
+      let!(:first_league) { Fabricate(:league) }
+      let!(:second_league) { Fabricate(:league) }
+
+      it "returns true for second ordered league" do
+        expect(first_league.last?).to eq(false)
+        expect(second_league.last?).to eq(true)
+      end
+    end
+
+    context "when row_order_rank is nil" do
+      let(:league) { Fabricate(:league) }
+
+      before { allow(league).to receive(:row_order_rank) { nil } }
+
+      it "returns false" do
+        expect(league.last?).to eq(false)
+      end
+    end
+  end
 end
