678f94efbef8a73a03af42c761f18813f508953e
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-07-15 15:39:38 -0700

    Refactor ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,26 +10,9 @@ class Ability
 
     self.user = input_user
     if user.admin?
-      can :manage, Contract
-      can :manage, League
-      can :manage, Lineup
-      can :manage, Team
-      can :manage, User
-      can :manage, Player
-      can :manage, Spot
-      cannot :destroy, User, id: user_id
+      admin_abilities
     else
-      can :read, User
-      can :update, User, id: user_id
-      can :read, League
-      can :read, Team
-      can :read, Contract
-      can :read, Player
-      can :read, Lineup
-      can :read, Spot
-      can :update, Team, user_id: user_id
-      can :manage, Lineup, team: { user_id: user_id }
-      can :manage, Spot, lineup: { team: { user_id: user_id } }
+      user_abilities
     end
   end
 
@@ -37,5 +20,22 @@ class Ability
 
     def user_id
       @user_id ||= user&.id
+    end
+
+    def admin_abilities
+      [Contract, League, Lineup, Player, Spot, Team, User].each do |class_name|
+        can :manage, class_name
+      end
+      cannot :destroy, User, id: user_id
+    end
+
+    def user_abilities
+      [Contract, League, Lineup, Player, Spot, Team, User].each do |class_name|
+        can :read, class_name
+      end
+      can :update, User, id: user_id
+      can :update, Team, user_id: user_id
+      can :manage, Lineup, team: { user_id: user_id }
+      can :manage, Spot, lineup: { team: { user_id: user_id } }
     end
 end
