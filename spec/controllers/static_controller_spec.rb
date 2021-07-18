1ac5003e128f7a8372fbd607227b03c3e0d48529
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-07-17 18:20:45 -0700

    Update StaticControllerSpec

--- spec/controllers/static_controller_spec.rb
+++ spec/controllers/static_controller_spec.rb
@@ -4,10 +4,27 @@ require "rails_helper"
 
 RSpec.describe StaticController, type: :controller do
   let(:user) { Fabricate(:user) }
+  let(:admin) { Fabricate(:admin) }
 
   describe "GET #unauthorized" do
+    context "for a guest" do
+      it "returns a success response" do
+        get :unauthorized
+        expect(response).to redirect_to(:new_user_session)
+      end
+    end
+
     context "for a user" do
       before { sign_in(user) }
+
+      it "returns a success response" do
+        get :unauthorized
+        expect(response).to be_successful
+      end
+    end
+
+    context "for an admin" do
+      before { sign_in(admin) }
 
       it "returns a success response" do
         get :unauthorized
