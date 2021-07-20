32959135cde86040187ae0ed3a4c6cffe348ebc4
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2021-07-19 21:26:19 -0700

    Add user dropdown

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 module UsersHelper
-  def log_out_link
-    link_to 'Log Out', destroy_user_session_path,
-            method: :delete, class: 'button button-warning button-clear'
-  end
-
   def user_tags(user)
     return unless user.admin?
 
@@ -19,4 +14,43 @@ module UsersHelper
     end
     safe_join(navitize(team_links), ', ')
   end
+
+  def user_dropdown(user)
+    options = { class: 'dropdown-menu user-dropdown',
+                data: { link: 'user-dropdown-link' } }
+
+    content_tag :div, options do
+      concat dropdown_menu_user_container(user)
+      concat dropdown_menu_logout_container
+    end
+  end
+
+  private
+
+    def dropdown_menu_user_container(user)
+      content_tag :div, class: 'dropdown-menu-container' do
+        concat content_tag(:p, user.email,
+                           class: 'user-email dropdown-menu-title')
+        concat safe_join(navitize(user_nav_links(user),
+                                  class: 'button button-clear'))
+      end
+    end
+
+    def user_nav_links(user)
+      links = [['My Teams', user_path(user)], ['All Users', users_path]]
+      return links unless can?(:update, user)
+
+      links.append ['User Settings', edit_user_path(user)]
+    end
+
+    def dropdown_menu_logout_container
+      content_tag :div, class: 'dropdown-menu-container' do
+        safe_join(navitize(log_out_link))
+      end
+    end
+
+    def log_out_link
+      [['Log Out', destroy_user_session_path,
+        { method: :delete, class: 'button button-warning button-clear' }]]
+    end
 end
---
+++ app/javascript/packs/dropdowns.js
@@ -0,0 +1,95 @@
+class Dropdown {
+  constructor(elem) {
+    this.dropdown = elem;
+    this.activeClass = 'active';
+    const linkSelector = elem.dataset.link;
+    this.link = document.querySelector(`.${linkSelector}`);
+    if (!this.valid()) {
+      return;
+    }
+
+    this.watchLink();
+  }
+
+  valid() {
+    return !!this.link;
+  }
+
+  dropdownElem(elem) {
+    return (elem &&
+            (elem.classList.contains('dropdown-menu') ||
+             elem.classList.contains('dropdown-link'))) ||
+            (elem.parentNode &&
+             elem.parentNode.classList.contains('dropdown-menu'));
+  }
+
+  watchLink() {
+    const dropdown = this;
+
+    this.link.addEventListener('click', function(event) {
+      event.preventDefault();
+      dropdown.toggle();
+    })
+
+    document.addEventListener('click', function(event) {
+      const target = event.target;
+      if (!dropdown.dropdownElem(target)) {
+        dropdown.toggleOff();
+      }
+    })
+  }
+
+  toggle() {
+    this.dropdown.style.top = this.positionTop();
+    this.dropdown.style.right = this.positionRight();
+    this.dropdown.style.left = null;
+    this.dropdown.classList.toggle(this.activeClass);
+    this.link.classList.toggle(this.activeClass);
+    this.dropdown.classList.remove('dropdown-menu-left');
+    if (this.dropdown.offsetLeft < 0) {
+      this.dropdown.style.right = null;
+      this.dropdown.style.left = this.positionLeft();
+      this.dropdown.classList.add('dropdown-menu-left');
+    }
+  }
+
+  toggleOff() {
+    this.dropdown.classList.remove(this.activeClass);
+    this.link.classList.remove(this.activeClass);
+  }
+
+  positionTop() {
+    const top = this.link.offsetTop;
+    const height = this.link.offsetHeight;
+    return `${top + height}px`;
+  }
+
+  positionRight() {
+    const left = this.link.offsetLeft;
+    const width = this.link.offsetWidth;
+    const windowWidth = window.innerWidth;
+    return `${windowWidth - (left + width + 15)}px`;
+  }
+
+  positionLeft() {
+    const left = this.link.offsetLeft;
+    return `${left - 15}px`;
+  }
+}
+
+var dropdowns = [];
+
+document.addEventListener('turbolinks:load', function() {
+  for (var elem of document.querySelectorAll('.dropdown-menu')) {
+    console.log(elem);
+    dropdowns.push(new Dropdown(elem));
+  }
+})
+
+// close any current dropdowns
+document.addEventListener('turbolinks:visit', function() {
+  for (var dropdown of dropdowns) {
+    dropdown.toggleOff();
+  }
+  dropdowns = [];
+})
